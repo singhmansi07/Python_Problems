11bacc57ab6da86fcdb68d4e22cf7efa37546a0f
Mansi Singh <81414330+singhmansi07@users.noreply.github.com>
2021-08-31 13:44:32 +0530

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,15 +1,12 @@
-def isPalindrome(num):
-    return str(num) == str(num)[::-1]
-
-
-def largest(min, max):
-    z = 0
-    for i in range(max, min, -1):
-        for j in range(max, min, -1):
-            if isPalindrome(i*j):
-                if i*j > z:
-                    z = i*j
-    return z
-
-
-print(largest(100, 999))
+n= int(input("Enter your number:"))
+temp = n 
+rev = 0 
+while (n>0):
+    d = n%10
+    rev = rev*10+d
+    n = n//10
+    
+if(temp==rev):
+    print("The Number is PALINDROME.")
+else:
+    print("The Number is NOT PALINDROME.")
